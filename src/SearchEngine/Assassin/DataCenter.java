bc54e4f098ca5d74c560fbbf65caafa71e875b8a
amaliujia <amaliujia@163.com>
2014-09-21 21:40:38 -0400

    add DataCenter singleton

---
+++ src/SearchEngine/Assassin/DataCenter.java
@@ -0,0 +1,24 @@
+package SearchEngine.Assassin;
+
+/**
+ * Created by amaliujia on 14-9-21.
+ */
+public class DataCenter {
+    public static DataCenter  dataCenter = null;
+
+    public DocLengthStore docLengthStore;
+    public int numDocs;
+    public int avgLenDoc;
+    public static double k1;
+    public static double b;
+    public static double k3;
+
+
+    public static DataCenter sharedDataCenter(){
+       if(dataCenter == null)
+           dataCenter = new DataCenter();
+       return dataCenter;
+    }
+
+
+}
