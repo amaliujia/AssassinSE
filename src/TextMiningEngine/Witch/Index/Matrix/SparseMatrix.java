49de37ccb0f34ea968f5cce825534e4787722109
amaliujia <amaliujia@163.com>
2015-02-28 14:34:36 -0500

    [Matrix]

--- src/TextMiningEngine/Witch/Index/Matrix/SparseMatrix.java
+++ src/TextMiningEngine/Witch/Index/Matrix/SparseMatrix.java
@@ -1,9 +1,23 @@
 package TextMiningEngine.Witch.Index.Matrix;
 
+import com.sun.tools.javac.util.Pair;
+
+import java.util.HashMap;
+
 /**
  * Created by amaliujia on 15-2-22.
  */
 public class SparseMatrix implements Matrix {
+    public HashMap<Pair<Integer, Integer>, Double> matrix;
+    public int rows;
+    public int columns;
+
+    public SparseMatrix(){
+        matrix = new HashMap<Pair<Integer, Integer>, Double>();
+        rows = 0;
+        columns = 0;
+    }
+
     @Override
     public Matrix mulMatrix(Matrix matrix) {
         return null;
---
+++ src/TextMiningEngine/Witch/Index/PageRank/PageRankSparseMatrix.java
@@ -0,0 +1,35 @@
+package TextMiningEngine.Witch.Index.PageRank;
+
+import TextMiningEngine.Witch.Index.Matrix.SparseMatrix;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by amaliujia on 15-2-22.
+ */
+public class PageRankSparseMatrix extends SparseMatrix {
+    public PageRankSparseMatrix(int rows, int columns, String path){
+        super();
+        this.rows = rows;
+        this.columns = columns;
+
+        setPageRankSparseMatrix(path);
+    }
+
+    public void setPageRankSparseMatrix(String path){
+        try {
+            Scanner scanner = new Scanner(new File(path));
+            String line = "";
+            while (scanner.hasNext()){
+                line = scanner.nextLine();
+                String[] s = line.split(" ");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
