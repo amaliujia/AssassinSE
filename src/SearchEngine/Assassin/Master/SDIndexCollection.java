450855315150c06fdfede946ab9a4de762507998
xiliuhk <xiliuhk@gmail.com>
2015-08-21 17:22:03 -0400

    [DSE] add declen into index collection.

--- src/SearchEngine/Assassin/Master/SDIndexCollection.java
+++ src/SearchEngine/Assassin/Master/SDIndexCollection.java
@@ -2,6 +2,7 @@ package SearchEngine.Assassin.Master;
 
 import SearchEngine.Assassin.Lucene.DocLengthStore;
 import SearchEngine.Assassin.Slave.SDSlaveIndexReader;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.NumericDocValues;
 
 import java.io.IOException;
@@ -15,7 +16,7 @@ import java.util.Map;
  */
 public class SDIndexCollection implements Serializable {
     private int numOfDocs;
-    private Map<String, NumericDocValues> docLenMap;
+    private Map<String, Map<Integer, Long>> docLenMap;
     private Map<String, Double> avgDocLenCntMap;
     private Map<String, Integer> fieldDocCntMap;
     private Map<String, Long> fieldLenMap;
@@ -25,21 +26,30 @@ public class SDIndexCollection implements Serializable {
 
 
         this.numOfDocs = sr.READER.numDocs();
-        avgDocLenCntMap = new HashMap<String, Double>();
-        fieldDocCntMap = new HashMap<String, Integer>();
-        fieldLenMap = new HashMap<String, Long>();
+        this.avgDocLenCntMap = new HashMap<String, Double>();
+        this.fieldDocCntMap = new HashMap<String, Integer>();
+        this.fieldLenMap = new HashMap<String, Long>();
+        this.docLenMap = new HashMap<String, Map<Integer, Long>>();
+        DocLengthStore dls = new DocLengthStore(sr.READER);
 
-        DocLengthStore dls;
-            dls = new DocLengthStore(sr.READER);
-            this.docLenMap = dls.getLengthMap();
-            for (String field : fields){
-                int fieldDocCnt = sr.READER.getDocCount(field);
-                long fieldLen = sr.READER.getSumTotalTermFreq(field);
-                double avgDocLen = 1.0*fieldLen/fieldDocCnt;
-                this.avgDocLenCntMap.put(field, avgDocLen);
-                this.fieldDocCntMap.put(field, fieldDocCnt);
-                this.fieldLenMap.put(field, fieldLen);
+        for (String field : fields){
+            int fieldDocCnt = sr.READER.getDocCount(field);
+            long fieldLen = sr.READER.getSumTotalTermFreq(field);
+            double avgDocLen = 1.0*fieldLen/fieldDocCnt;
+            Map<Integer, Long> fieldDocLenMap = new HashMap<Integer, Long>();
+            for (int docid = 0; docid < sr.READER.maxDoc(); docid++){
+                if (fieldDocLenMap.containsKey(docid)){
+                    continue;
+                }else{
+                    fieldDocLenMap.put(docid, dls.getDocLength(field, docid));
+                }
             }
+            this.docLenMap.put(field, fieldDocLenMap);
+            this.avgDocLenCntMap.put(field, avgDocLen);
+            this.fieldDocCntMap.put(field, fieldDocCnt);
+            this.fieldLenMap.put(field, fieldLen);
+        }
+
     }
 
     public long getFieldLength(String field){
@@ -68,6 +78,12 @@ public class SDIndexCollection implements Serializable {
     }
 
     public double getDocLength(String field, int docid){
+        if (!this.docLenMap.containsKey(field)){
+            return 0;
+        }
+        if (!this.docLenMap.get(field).containsKey(docid)){
+            return 0;
+        }
         return this.docLenMap.get(field).get(docid);
     }
 }
